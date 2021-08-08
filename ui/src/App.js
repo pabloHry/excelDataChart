764abef93a3767d06050cbcfaba6b653708a47f5
Pablo <pablohanry9@gmail.com>
2021-08-08 12:53:36 +0200

    end

---
+++ ui/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import Chart from "./components/chart";
+
+function App() {
+  return (
+    <div className='App'>
+      <Chart />
+    </div>
+  );
+}
+
+export default App;
